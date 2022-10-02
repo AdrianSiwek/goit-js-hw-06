b22555bb70c144b997747a0eb9612157446ae079
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-02 19:08:32 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const gallery = document.querySelector('.gallery');
+const newImg = images
+  .map((image) => `<li><img class = "image" src="${image.url}" alt="${image.alt}" /></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImg);
