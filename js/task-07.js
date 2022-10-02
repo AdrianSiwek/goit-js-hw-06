21e2373356fd0e522a5f2d06ced007bf6bbb15be
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-03 00:42:24 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,7 @@
-const input = document.getElementById
+const input = document.getElementById("font-size-control");
+const text = document.getElementById("text");
+
+input.addEventListener("input", () => {
+    const value = input.value;
+    text.style.fontSize = `${value}px`;
+});
