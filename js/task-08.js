9dd414892c0b9d725b6734ea7390b63ec76d6de4
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-03 00:56:10 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        return console.log("Proszę uzupełnić wszystkie pola!");
+    } else {
+        else {
+            const result = { email: email.value, password: password.value };
+            console.log(result);
+        }
+        event.currentTarget.reset();
+    }
+}
