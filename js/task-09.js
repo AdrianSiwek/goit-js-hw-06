25ebbd8024477290c5088a656595981783036590
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-03 01:17:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const variableBtn = bodyColor.querySelector(".change-color");
+const color = bodyColor.querySelector(".color");
+
+const changeColor = () => {
+  const variableColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = `${variableColor}`;
+  color.textContent = `${variableColor}`
+};
+
+variableBtn.addEventListener("click", changeColor);
